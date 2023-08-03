f318a0a48b0e27eaf8ee03c542ccc47dd0f80799
emobilis <ttmugo65@gmail.com>
2023-08-03 13:03:12 +0300

    Loops and Conditional Expressions

---
+++ src/main/kotlin/ConditionalExpressions.kt
@@ -0,0 +1,34 @@
+fun main(args: Array<String>) {
+    var marks = 100
+    var grade =
+        if (marks <= 40){
+            "E"
+        }else if (marks <=50){
+            "D"
+        }else if (marks <=60){
+            "C"
+        }else if (marks <=70){
+            "B"
+        }else {
+            "A"
+        }
+    println(grade)
+
+    var bettingNumber = 0
+    var bettingResult = when(bettingNumber){
+        1 -> {
+            "You won a house"
+        }   2 -> {
+            "You won a cat"
+        }   3 -> {
+            "You won a dog"
+        }   4 -> {
+            "You won a million"
+        }
+        else ->{
+            "Enter a number between 1 -  4 to bet."
+        }
+    }
+    println(bettingResult)
+    
+}
--- src/main/kotlin/ConditionalStatements.kt
+++ src/main/kotlin/ConditionalStatements.kt
@@ -15,6 +15,41 @@ fun main(args: Array<String>) {
         println("TRUE")
     }
 
+    val marks = 100
+    if (marks <= 40){
+        println("E")
+    }else if (marks <=50){
+        println("D")
+    }else if (marks <=60){
+        println("C")
+    }else if (marks <=70){
+        println("B")
+    }else {
+        println("A")
+    }
+
+    var bettingNumber = 0
+    when(bettingNumber){
+        1 -> {
+            println("You won a house")
+        }   2 -> {
+            println("You won a cat")
+        }   3 -> {
+            println("You won a dog")
+        }   4 -> {
+            println("You won a million")
+        }
+        else ->{
+            println("Enter a number between 1 -  4 to bet.")
+        }
+    }
+
+
+
+
+
+
+
 }
 
  
